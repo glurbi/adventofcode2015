f135f12f84ac76b41889fc20a71f08a8149ce456
Vincent Gay <vincent.gay@cognite.com>
2020-07-05 11:40:15 +0200

    day1.1

--- day1.go
+++ day1.go
@@ -2,7 +2,7 @@ package day1
 
 import "fmt"
 
-func Floor(s string) int {
+func Floor1(s string) int {
 	floor := 0
 	for _, c := range s {
 		if c == '(' {
@@ -14,4 +14,21 @@ func Floor(s string) int {
 		}
 	}
 	return floor
+}
+
+func Floor2(s string) int {
+	floor := 0
+	for p, c := range s {
+		if c == '(' {
+			floor += 1
+		} else if c == ')' {
+			floor -= 1
+		} else {
+			panic(fmt.Sprintf("Invalid character %v", c))
+		}
+		if floor == -1 {
+			return p+1
+		}
+	}
+	panic(fmt.Sprintf("Not going to basement"))
 }
--- day1_test.go
+++ day1_test.go
@@ -4,27 +4,47 @@ import "fmt"
 import "testing"
 import "io/ioutil"
 
-func TestFloor(t *testing.T) {
-	test(t, "(())", 0)
-	test(t, "()()", 0)
-	test(t, "(((", 3)
-	test(t, "(()(()(", 3)
-	test(t, "())", -1)
-	test(t, "))(", -1)
-	test(t, ")))", -3)
-	test(t, ")())())", -3)
-}
-
-func TestFloorMain(t *testing.T) {
+func input() string {
 	b, err := ioutil.ReadFile("day1.txt")
     if err != nil {
         fmt.Print(err)
 	}
-	fmt.Printf("Day 1 - %v\n", Floor(string(b)))
+	return string(b)
 }
 
-func test(t *testing.T, s string, want int) {
-    if got := Floor(s); got != want {
-        t.Errorf("Floor(%s) = %v, want %v", s, got, want)
+func test1(t *testing.T, s string, want int) {
+    if got := Floor1(s); got != want {
+        t.Errorf("Floor1(%s) = %v, want %v", s, got, want)
     }
 }
+
+func TestFloor1(t *testing.T) {
+	test1(t, "(())", 0)
+	test1(t, "()()", 0)
+	test1(t, "(((", 3)
+	test1(t, "(()(()(", 3)
+	test1(t, "())", -1)
+	test1(t, "))(", -1)
+	test1(t, ")))", -3)
+	test1(t, ")())())", -3)
+}
+
+func TestFloorMain1(t *testing.T) {
+	fmt.Printf("Day 1.1 => %v\n", Floor1(input()))
+}
+
+func test2(t *testing.T, s string, want int) {
+    if got := Floor2(s); got != want {
+        t.Errorf("Floor2(%s) = %v, want %v", s, got, want)
+    }
+}
+
+func TestFloor2(t *testing.T) {
+	test2(t, ")", 1)
+	test2(t, "()())", 5)
+}
+
+func TestFloorMain2(t *testing.T) {
+	fmt.Printf("Day 1.2 => %v\n", Floor2(input()))
+}
+
